592487227b03634b122669ecfef080cfd68b32c6
muryloandrades <muryloandrades@gmail.com>
2023-01-03 13:55:59 -0300

    |+| Args do ComboBox

--- src/components/ComboBox/ComboBoxDefault/ComboBox.stories.tsx
+++ src/components/ComboBox/ComboBoxDefault/ComboBox.stories.tsx
@@ -5,6 +5,9 @@ import { ComboBoxDefault } from "./ComboBox";
 export default {
   title: "Components/ComboBoxS",
   component: ComboBoxDefault,
+  args: {
+    option: ["Example1", "Example2", "Example3", "Example4", "Example5"],
+  },
 } as Meta;
 
 export const ComboBox = {};
